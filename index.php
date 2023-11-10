8c4b71fbf8ce50c2cff3d45925c06f5cade05ea1
Triki Osama <trikiosama@gmail.com>
2023-11-10 22:30:50 +0100

    premier commit ```

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Small world calculator</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>Small world calculator</h1>
+        <section>
+            <div id="search_bar">
+                <form method="GET">
+                    <input type="text" id="search" name="search" placeholder="Search cards...">
+                    <button type="submit">Add Card</button>
+                </form>
+            </div>
+            <div id="deck">
+                <!-- Add 40 to 60 images here -->
+            </div>
+        </section>
+    </body>
+</html>
